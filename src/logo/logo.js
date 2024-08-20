bd3a44e38543719012784717f42436929d6a0554
mishobo <husseinabdalla287@gmail.com>
2024-08-20 19:04:29 +0300

    3 divs

--- src/logo/logo.js
+++ src/logo/logo.js
@@ -4,20 +4,19 @@ import Logo from '../images/White_on_Transparent.png'
 export default function logo() {
   return (
     <div className='container'>
-      <div></div>
-      <div className='content'>
+            <div className='content'>
         <div className='logo'>
           <img src={Logo}  alt='munene advocate logo' />
         </div>
       </div>
+      <div>
+        <h1>Precision, Integrity, Client Focus</h1>
+      </div>
       <div className='footer'> 
         <pre>
-          Munene Micheni, LLB(Hons) UoN, Dip. Law (KSL)<br/>
-          Managing Partner<br/>
-          <a href='mailto:munene@munenemicheniadvocates.com'>munene@munenemicheniadvocates.com</a><br/>
+           Kose Heights, 5th Floor, Hurlingham<br/>
           +254733529467, +254757336629<br/>
-          Kose Heights, 5th Floor, Hurlingham<br/>
-          P.O. Box 17164 - 00100, Nairobi, Kenya
+          <a href='mailto:munene@munenemicheniadvocates.com'>munene@munenemicheniadvocates.com</a><br/>
         </pre>
       </div>
     </div>
